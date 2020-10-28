a41a8ef6e776127fecc5b19d9fb1e9030205150b
Nobuyuki SAMBUICHI <nobuyuki@sambuichi.jp>
2020-10-28 09:37:45 +0900

    更新

--- データ検証/EIPA/xBRL/xBRL-GL2label.sh
+++ データ検証/EIPA/xBRL/xBRL-GL2label.sh
@@ -14,38 +14,40 @@ Tmp=/tmp/${0##*/}.$$
 # === Log ============================================================
 exec 2>log/${0##*/}.$$.log
 # === tsv -> xml ============================================================
+# seq parent code level type module term description
+# 1   2      3    4     5    6      7    8
 # see https://stackoverflow.com/questions/8024392/awk-replace-a-column-with-its-hash-value
-cat eipa/source/EN_16931-1.txt | awk -F'\t' '{
-  tmp="echo " $1 date " | openssl md5 | cut -f2 -d\" \"";
+cat gl/source/xBRL-GL.txt | awk -F'\t' '{
+  tmp="echo " $3 date " | openssl md5 | cut -f2 -d\" \"";
   tmp | getline cksum;
   close(tmp);
   print cksum "\t" $0;
 }' > $Tmp-cksum
-cat $Tmp-cksum | awk -F'\t' 'BEGIN {
+
+cat $Tmp-cksum | awk -F'\t' -v module=$1 'BEGIN {
   printf "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
   printf "<link:linkbase xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"";
   printf "  xmlns:link=\"http://www.xbrl.org/2003/linkbase\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"";
   printf "  xsi:schemaLocation=\"http://www.xbrl.org/2003/linkbase http://www.xbrl.org/2003/xbrl-linkbase-2003-12-31.xsd\">";
   printf "  <link:labelLink xlink:type=\"extended\" xlink:role=\"http://www.xbrl.org/2003/role/link\">\n";
 }
-{ 
-  if ("root"==$2 || match($2, /^B[GT]-[0-9]*$/) > 0) {
-    id=$2;
-    cksum="_" $1;
-    label="";
-    for(i=6;i<=NF;i++) label=label $i" "; 
-    printf "    <link:loc xlink:type=\"locator\" xlink:href=\"eipa-cen-2020-12-31.xsd#eipa-cen_%s\" xlink:label=\"%s%s\"/>\n", id, id, cksum;
-    printf "    <link:label xlink:type=\"resource\" xlink:label=\"label_%s%s\" xlink:role=\"http://www.xbrl.org/2003/role/label\" xml:lang=\"en\" id=\"label_%s%s\">%s</link:label>\n", id, cksum, id, cksum, label;
-    printf "    <link:labelArc xlink:type=\"arc\" xlink:arcrole=\"http://www.xbrl.org/2003/arcrole/concept-label\" xlink:from=\"%s%s\" xlink:to=\"label_%s%s\"/>\n", id, cksum, id, cksum;
-  }
+{
+  code=$4;
+  cksum="_" $1;
+  label="";
+  for(i=9;i<=NF;i++) label=label $i" "; 
+  # print code " " label;
+  printf "    <link:loc xlink:type=\"locator\" xlink:href=\"eipa-cen-2020-12-31.xsd#eipa-cen_%s\" xlink:label=\"%s%s\"/>\n", code, code, cksum;
+  printf "    <link:label xlink:type=\"resource\" xlink:label=\"label_%s%s\" xlink:role=\"http://www.xbrl.org/2003/role/label\" xml:lang=\"en\" code=\"label_%s%s\">%s</link:label>\n", code, cksum, code, cksum, label;
+  printf "    <link:labelArc xlink:type=\"arc\" xlink:arcrole=\"http://www.xbrl.org/2003/arcrole/concept-label\" xlink:from=\"%s%s\" xlink:to=\"label_%s%s\"/>\n", code, cksum, code, cksum;
 }
 END {
   printf " </link:labelLink>\n";
   printf "</link:linkbase>";
-}' > eipa/source/xBRL_label.xml
+}' > gl/source/gl-"$1"-2020-12-31-label.xml
 
 # --------------------------------------------------------------------
 rm $Tmp-*
 rm log/${0##*/}.$$.*
 exit 0
-# text2label.sh
+# xBRL-GL2label.sh
---
+++ データ検証/EIPA/xBRL/xBRL-GL2presentation.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+# === Initialize shell environment ===================================
+set -euvx
+# -e  Exit immediately if a command exits with a non-zero status.
+# -u  Treat unset variables as an error when substituting.
+# -v  Print shell input lines as they are read.
+# -x  Print commands and their arguments as they are executed.
+export LC_ALL=C
+type command >/dev/null 2>&1 && type getconf >/dev/null 2>&1 &&
+export PATH=".:./bin:../bin:$(command -p getconf PATH)${PATH+:}${PATH-}"
+export UNIX_STD=2003  # to make HP-UX conform to POSIX
+# === temporary file prefix ==========================================
+Tmp=/tmp/${0##*/}.$$
+# === Log ============================================================
+exec 2>log/${0##*/}.$$.log
+# === tsv -> xpath ============================================================
+# seq parent code level type module term description
+# 1   2      3    4     5    6      7    8
+cat gl/source/xBRL-GL.txt | awk -F'\t' -v module=$1 'BEGIN {
+  n=0;
+  parent="root";
+  curent_level=1;
+  currents[1]="";
+  currents[2]="";
+  currents[3]="";
+  currents[4]="";
+  currents[5]="";
+  currents[6]="";
+  currents[7]="";
+  print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+  print "<!-- (c) XBRL Japan -->";
+  print "<linkbase xmlns=\"http://www.xbrl.org/2003/linkbase\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.xbrl.org/2003/linkbase http://www.xbrl.org/2003/xbrl-linkbase-2003-12-31.xsd\">";
+  print "  <presentationLink xlink:type=\"extended\" xlink:role=\"http://www.xbrl.org/2003/role/link\">";
+}
+{
+  if (module==$6) {
+    code=$3;
+    level=$4;
+    term=$7
+    currents[level]=code;
+    if (1 == level) {
+      parent="root";
+    }
+    else {
+      parent=currents[level - 1];
+    }
+    if (level < current_level) {
+      for (i = level + 1; i < 8; i++) {
+        currents[i]="";
+      }
+    }
+    current_level=level;
+    terms[code]=term;
+    p_module=substr(parent, 1, 3);
+    if ("src"==p_module) p_module="srcd";
+    # print n " " code " parent module=" p_module " parent=" parent " level=" level " [1]" currents[1] " [2]" currents[2] " [3]" currents[3] " [4]" currents[4] " [5]" currents[5] " [6]" currents[6] " [7]" currents[7];
+    if (1!=parents[parent]) {
+      parents[parent]=1;
+      if (p_module==module) {
+        printf "    <loc xlink:type=\"locator\" xlink:href=\"gl-%s-2020-12-31.xsd#gl-%s_%s\" xlink:label=\"gl-%s_%s\" xlink:title=\"presentation parent: %s\"/>\n", p_module, p_module, terms[parent], p_module, terms[parent], terms[parent];
+      } else {
+        printf "    <loc xlink:type=\"locator\" xlink:href=\"../gl-%s-2020-12-31.xsd#gl-%s_%s\" xlink:label=\"gl-%s_%s\" xlink:title=\"presentation parent: %s\"/>\n", p_module, p_module, terms[parent], p_module, terms[parent], terms[parent];
+      }
+    }
+    printf "    <loc xlink:type=\"locator\" xlink:href=\"gl-%s-2020-12-31.xsd#gl-%s_%s\" xlink:label=\"gl-%s_%s\" xlink:title=\"presentation child: %s\"/>\n", module, module, term, module, term, term;
+    printf "    <presentationArc xlink:type=\"arc\" xlink:arcrole=\"http://www.xbrl.org/2003/arcrole/parent-child\" xlink:from=\"gl-%s_%s\" xlink:to=\"gl-%s_%s\" xlink:title=\"presentation: %s to %s\" use=\"optional\" order=\"%s\"/>\n", p_module, terms[parent], module, term, terms[parent], term, n;
+    n=n+1;
+  }
+}
+END {
+  print "  </presentationLink>";
+  print "</linkbase>"; 
+}' > gl/source/gl-"$1"-2020-12-31-presentation.xml
+# --------------------------------------------------------------------
+# rm $Tmp-*
+rm log/${0##*/}.$$.*
+exit 0
+# xBRL-GL2presentaton.sh
