8d1f85c134107802359432e31c1a198a157c0375
Nobuyuki SAMBUICHI <nobuyuki@sambuichi.jp>
2021-06-07 15:02:00 +0900

    Update

--- pint_ja/dic2etree/__init__.py
+++ pint_ja/dic2etree/__init__.py
@@ -11,7 +11,10 @@ ns = {
   'cbc': 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2',
   'qdt': 'urn:oasis:names:specification:ubl:schema:xsd:QualifiedDataTypes-2',
   'udt': 'urn:oasis:names:specification:ubl:schema:xsd:UnqualifiedDataTypes-2',
-  'ccts': 'urn:un:unece:uncefact:documentation:2'
+  'ccts': 'urn:un:unece:uncefact:documentation:2',
+  'ubl-creditnote': 'urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2',
+  'ubl-invoice': 'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2',
+  'sch': 'http://purl.oclc.org/dsdl/schematron'
 }
 
 __all__ = ['dict_to_etree', 'etree_to_dict', 'get_path_value', 'set_path_value', 'ns']
